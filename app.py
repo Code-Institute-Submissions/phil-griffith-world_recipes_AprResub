9a4b157478fe0484273d685023c6cf6d915dde2e
phil-griffith <philgriffith@outlook.com>
2021-04-24 18:16:36 +0000

    Add 404 redirect page

--- app.py
+++ app.py
@@ -644,6 +644,12 @@ def manage_recipes():
         return redirect(url_for('sign_in'))
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
